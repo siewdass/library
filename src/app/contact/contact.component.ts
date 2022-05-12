3c756a90f5c2ec8941b0917a52e415e5aee6df47
siewdass <siewdass@gmail.com>
2022-05-11 23:11:10 -0400

    -

--- src/app/contact/contact.component.ts
+++
@@ -1,6 +0,0 @@
-import { Template, Style } from '../../../framework/component'
-
-@Template( 'contact.component.pug' )
-@Style( 'contact.component.less' )
-export class ContactComponent {
-}
--- src/app/main.ts
+++
@@ -1,1 +0,0 @@
-let VARIABLE: string = 'Hello World'
