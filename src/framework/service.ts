a08469a5ca3a8add38e91b81378344f0cfc5618a
siewdass <siewdass@gmail.com>
2022-05-18 12:51:05 -0400

    -

--- src/framework/service.ts
+++ src/framework/service.ts
@@ -2,13 +2,12 @@ export function SERVICE( ) {
   return function( target: any ) {
     target.prototype.service = true
     if ( !global.services ) {
-      global.services = {}
+      global.services = { }
     }
     const service = new target
     global.services[ service.constructor.name ] = service
   }
 }
-
 
 export function service( target: any, key: string): any {
   const type = Reflect.getMetadata( 'design:type', target, key )
